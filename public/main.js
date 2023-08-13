10427049b955e03c551f8892122a70eb14d2077b
KellyDanielO <kizdver@gmail.com>
2023-08-13 19:06:00 +0100

    .

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
-
-var slide_menu_btn = document.getElementById("slide_menu_btn");
+var slide_menu_btn = document.querySelector("#slide_menu_btn");
 slide_menu_btn.addEventListener("click", () => {
     let slide_menu = document.querySelector(".slide_menu");
     slide_menu.classList.add("active");
-    console.log('yes');
+    // console.log('yes');
+});
+var side_close_btn = document.querySelector("#side_close_btn");
+side_close_btn.addEventListener("click", () => {
+    let slide_menu = document.querySelector(".slide_menu");
+    slide_menu.classList.remove("active");
+    // console.log('yes');
 });
